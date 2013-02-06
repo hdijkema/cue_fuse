8872620d32c5e7bd02ec1d2f551ab0a3815b83d8
Hans Oesterholt <hans@oesterholt.net>
2013-02-07 00:16:15 +0100

    GNU license messages

--- src/mp3cuefuse.c
+++ src/mp3cuefuse.c
@@ -2,12 +2,12 @@
    This file is part of mp3cuefuse.
    Copyright 2013, Hans Oesterholt <debian@oesterholt.net>
 
-   Clementine is free software: you can redistribute it and/or modify
+   mp3cuefuse is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
-   Clementine is distributed in the hope that it will be useful,
+   mp3cuefuse is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
